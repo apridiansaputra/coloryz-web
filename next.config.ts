41796232cada130a86e308ce0d49bcbc0c9594f4
Apridian Saputra <saputrapridian07@gmail.com>
2025-11-29 22:38:13 +0700

    edit erorr 1: external package config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
+  serverExternalPackages: ['onnxruntime-node', 'sharp'],
+  
+  // experimental: {
+  //   serverComponentsExternalPackages: ['onnxruntime-node', 'sharp'],
+  // },
 };
 
 export default nextConfig;
